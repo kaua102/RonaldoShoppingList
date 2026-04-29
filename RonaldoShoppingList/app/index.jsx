f8dbb21f25d3245234dea84203f1350dbfac47bd
kaua102 <kaua26987@gmail.com>
2026-04-29 11:35:14 -0300

    2

---
+++ RonaldoShoppingList/app/index.jsx
@@ -0,0 +1,21 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function Welcome() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerImage}>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
